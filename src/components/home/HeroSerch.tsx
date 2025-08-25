6e4266426bc842269a919f7fc51f1e7ea70fd810
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2025-08-26 00:22:19 +0600

    complete hero section

---
+++ src/components/home/HeroSerch.tsx
@@ -0,0 +1,148 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+export default function HeroSearch({ setOpen, open }: { setOpen: React.Dispatch<React.SetStateAction<boolean>>, open: boolean }) {
+    const [selectedBloodGroup, setSelectedBloodGroup] = useState("none");
+
+    const [selectedDivision, setSelectedDivision] = useState<{ id: number, name: string } | null>(null);
+    const [selectedDistrict, setSelectedDistrict] = useState<{ id: number, name: string } | null>(null);
+    const [selectedUpazila, setSelectedUpazila] = useState<{ id: number, name: string } | null>(null);
+
+    const [division, setDivision] = useState<{ id: number, name: string, bn_name: string }[]>([]);
+    const [district, setDistrict] = useState<{ id: number, division_id: number, name: string, bn_name: string }[]>([]);
+    const [upazila, setUpazila] = useState<{ id: number, district_id: number, name: string, bn_name: string }[]>([]);
+
+    useEffect(() => {
+        fetch('/api/location/division.json')
+            .then(res => res.json())
+            .then(data => setDivision(data));
+
+        fetch('/api/location/districts.json')
+            .then(res => res.json())
+            .then(data => setDistrict(data));
+
+        fetch('/api/location/upazila.json')
+            .then(res => res.json())
+            .then(data => setUpazila(data));
+
+
+    }, []);
+
+    return (
+        <div className="card-body">
+            <fieldset className="fieldset w-full">
+                {/* Blood Group */}
+                <div className="flex filter overflow-hidden w-full gap-y-1">
+                    <input
+                        className="btn btn-sm rounded-lg filter-reset bg-neutral text-white text-xl "
+                        type="radio"
+                        name="bloodGroup"
+                        onChange={() => setSelectedBloodGroup("none")}
+                        aria-label={"none"}
+                        value={"none"}
+                    />
+                    {["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"].map(bg => (
+                        <input
+                            key={bg}
+                            className="btn btn-sm rounded-lg"
+                            type="radio"
+                            name="bloodGroup"
+                            onChange={() => setSelectedBloodGroup(bg)}
+                            aria-label={bg}
+                            value={bg}
+                        />
+                    ))}
+                </div>
+
+                {/* Division + District */}
+                <div className="flex gap-x-2 w-full mt-3">
+                    {/* Division */}
+                    <div className="w-full">
+                        <label className="label my-1">Division</label>
+                        <select
+                            value={selectedDivision?.id ?? ""}
+                            className="select"
+                            onChange={(e) => {
+                                const id = Number(e.target.value);
+                                const divi = division.find(d => d.id === id);
+                                if (divi) {
+                                    setSelectedDivision({ id: divi.id, name: divi.bn_name });
+                                    setSelectedDistrict(null);
+                                    setSelectedUpazila(null);
+                                }
+                            }}
+                        >
+                            <option value="" disabled>Select Division</option>
+                            {division.map(divi => (
+                                <option key={divi.id} value={divi.id}>{divi.bn_name}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* District */}
+                    <div className="w-full">
+                        <label className="label my-1">District</label>
+                        <select
+                            value={selectedDistrict?.id ?? ""}
+                            className="select"
+                            disabled={!selectedDivision}
+                            onChange={(e) => {
+                                const id = Number(e.target.value);
+                                const dist = district.find(d => d.id === id);
+                                if (dist) {
+                                    setSelectedDistrict({ id: dist.id, name: dist.bn_name });
+                                    setSelectedUpazila(null);
+                                }
+                            }}
+                        >
+                            <option value="" disabled>Select District</option>
+                            {district
+                                .filter(d => d.division_id === selectedDivision?.id)
+                                .map(d => (
+                                    <option key={d.id} value={d.id}>{d.bn_name}</option>
+                                ))}
+                        </select>
+                    </div>
+                </div>
+
+                {/* Upazila */}
+                <div className="flex w-full mt-3">
+                    <span className="w-full flex flex-col ">
+                        <label className="label my-1">Upazila</label>
+                        <select
+                            value={selectedUpazila?.id ?? ""}
+                            className="select w-full"
+                            disabled={!selectedDistrict}
+                            onChange={(e) => {
+                                const id = Number(e.target.value);
+                                const upz = upazila.find(u => u.id === id);
+                                if (upz) {
+                                    setSelectedUpazila({ id: upz.id, name: upz.bn_name });
+                                }
+                            }}
+                        >
+                            <option value="" disabled>Select Upazila</option>
+                            {upazila
+                                .filter(u => u.district_id === selectedDistrict?.id)
+                                .map(u => (
+                                    <option key={u.id} value={u.id}>{u.bn_name}</option>
+                                ))}
+                        </select>
+                    </span>
+                </div>
+
+                <button className="btn  btn-neutral mt-4 rounded-xl"
+                    onClick={() => {
+                        setOpen(!open);
+                        console.log("Selected BloodGroup:", selectedBloodGroup);
+                        console.log("Selected Division:", selectedDivision);
+                        console.log("Selected District:", selectedDistrict);
+                        console.log("Selected Upazila:", selectedUpazila);
+                    }}
+                >
+                    Search
+                </button>
+            </fieldset>
+        </div>
+    )
+}
---
+++ src/components/home/HeroSerchDoner.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid, Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+export default function HeroSerchDoner() {
+    const doners = [
+        { id: 1, name: "Abdul Jabbar", age: 21, location: "Tongi Gazipur Dhaka Bangladesh.", bloodGroup: "O+", profileImage: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3BsYXlcLzBiN2Y0ZTliLWY1OWMtNDAyNC05ZjA2LWIzZGMxMjg1MGFiNy0xOTIwLTEwODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=" },
+        { id: 2, name: "Hasan", age: 24, location: "Dhaka, Bangladesh.", bloodGroup: "A+", profileImage: "https://randomuser.me/api/portraits/men/32.jpg" },
+        { id: 3, name: "Rahim", age: 20, location: "Gazipur, Bangladesh.", bloodGroup: "B-", profileImage: "https://randomuser.me/api/portraits/men/45.jpg" },
+        { id: 4, name: "Karim", age: 29, location: "Uttara, Dhaka.", bloodGroup: "AB+", profileImage: "https://randomuser.me/api/portraits/men/12.jpg" },
+        { id: 5, name: "Jabed", age: 26, location: "Mirpur, Dhaka.", bloodGroup: "O-", profileImage: "https://randomuser.me/api/portraits/men/27.jpg" },
+        { id: 6, name: "Nayeem", age: 23, location: "Banani, Dhaka.", bloodGroup: "A-", profileImage: "https://randomuser.me/api/portraits/men/66.jpg" },
+        { id: 7, name: "Sakib", age: 22, location: "Barishal, Bangladesh.", bloodGroup: "B+", profileImage: "https://randomuser.me/api/portraits/men/77.jpg" },
+        { id: 8, name: "Fahim", age: 25, location: "Cumilla, Bangladesh.", bloodGroup: "AB-", profileImage: "https://randomuser.me/api/portraits/men/88.jpg" },
+    ];
+
+    const bloodGroups = [
+        { name: "O+", img: "/svg/op.svg" },
+        { name: "O-", img: "/svg/on.svg" },
+        { name: "A+", img: "/svg/ap.svg" },
+        { name: "A-", img: "/svg/an.svg" },
+        { name: "B+", img: "/svg/bp.svg" },
+        { name: "B-", img: "/svg/bn.svg" },
+        { name: "AB+", img: "/svg/abp.svg" },
+        { name: "AB-", img: "/svg/abn.svg" }
+    ];
+
+    return (
+        <Swiper
+            slidesPerView={2}
+            spaceBetween={20}
+
+            pagination={{ clickable: true }}
+            breakpoints={{
+                0: {
+                    slidesPerView: 1,   // mobile এ ১টা card
+                    spaceBetween: 5,
+                    loop: false, width: 250, updateOnWindowResize: true // 👉 Mobile এ loop বন্ধ
+                },
+                320: {
+                    slidesPerView: 1,   // mobile এ ১টা card
+                    spaceBetween: 10,
+                    loop: false, width: 300, updateOnWindowResize: true     // 👉 Mobile এ loop বন্ধ
+                },
+                640: {
+                    slidesPerView: 2,   // tablet এ ২টা card
+                    spaceBetween: 15,
+                    loop: true, width: null
+                },
+                1024: {
+                    slidesPerView: 3,   // laptop এ ৩টা card
+                    spaceBetween: 20,
+                    loop: true, width: null
+                },
+                1280: {
+                    slidesPerView: 4,   // বড় screen এ ৪টা card
+                    spaceBetween: 25,
+                    loop: true, width: null
+                },
+            }}
+            modules={[Pagination]}
+            className="mySwiper container mx-auto"
+        >
+            {doners.map((doner) => (
+                <SwiperSlide className="py-6" key={doner.id}>
+                    <div className="card relative flex flex-col sm:h-56 h-96  sm:flex-row w-full card-side bg-base-100 shadow-sm">
+                        <figure>
+                            <img className="sm:w-32 w-full h-72 object-cover" src={doner.profileImage} />
+                        </figure>
+                        <div className="card-body relative">
+                            <div>
+                                <h2 className="card-title">{doner.name}</h2>
+                                <h2>Age: {doner.age}</h2>
+                            </div>
+                            <p>{doner.location}</p>
+                            <span className="w-11 absolute top-4 right-4">
+                                <img
+                                    className="w-11"
+                                    src={bloodGroups.find((bg) => bg.name === doner.bloodGroup)?.img}
+                                    alt={doner.bloodGroup}
+                                />
+                            </span>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-neutral rounded-lg btn-sm">View Contact</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+
+
+    );
+}
