f4abc50e0cfdc9385376b315bb2b4226884c8339
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2025-08-28 19:07:34 +0600

    Workflow added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'export',  
+  basePath: '/roktodaan',       // repo name
+  assetPrefix: '/roktodaan',   
  images: {
     remotePatterns: [new URL('https://randomuser.me/api/**')],
   },
